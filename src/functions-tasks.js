ec84c14678f6659a6ac612aa51a4ef53dedcee75
Anzhalika Turlak <293852647@mail.ru>
2024-04-14 12:51:54 +0300

    feat: partialUsingArguments and getIdGeneratorFunction

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -204,8 +204,10 @@ function logger(/* func, logFunc */) {
  *   partialUsingArguments(fn, 'a','b','c')('d') => 'abcd'
  *   partialUsingArguments(fn, 'a','b','c','d')() => 'abcd'
  */
-function partialUsingArguments(/* fn, ...args1 */) {
-  throw new Error('Not implemented');
+function partialUsingArguments(fn, ...args1) {
+  return function (...args2) {
+    return fn(...args1, ...args2);
+  };
 }
 
 /**
@@ -225,8 +227,13 @@ function partialUsingArguments(/* fn, ...args1 */) {
  *   getId4() => 7
  *   getId10() => 11
  */
-function getIdGeneratorFunction(/* startFrom */) {
-  throw new Error('Not implemented');
+function getIdGeneratorFunction(startFrom) {
+  let currentId = startFrom;
+  return function () {
+    const returnValue = currentId;
+    currentId += 1;
+    return returnValue;
+  };
 }
 
 module.exports = {
